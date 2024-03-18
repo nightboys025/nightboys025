bdf11516e7a903e58a109892780070769fd5b40f
nightboys025 <jiminfox2025@kakao.com>
2024-03-18 15:59:17 +0900

    1234

--- 123/index.js
+++ 123/index.js
@@ -1,18 +1,20 @@
-const oanvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillRect(0,0, canvas.width, canvas.height);
+const gravity = 0.2;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor(position, velocity){
+    constructor( { position, velocity })
+    {
         this.position = position;
-        
-       
-       this.velocity = this.velocity;
-       
+
+        this.velocity = velocity;
+
         this.width = 30;
         this.height = 150;
     }
@@ -22,49 +24,86 @@ class Sprite {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
-    updata(){
+    update() {
         this.draw();
-        
-        this.position.y += 10;
+
+        this.velocity.y += gravity;
+
+        this.position.y += this.velocity.y
+    
+        this.position.x += this.velocity.x
+
+        this.position.y += this.velocity.y;
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y =0;
+        }
+        else{
+            this.velocity.y += gravity;
+        }
     }
 }
-const player = new Sprite ({
+
+const player = new Sprite( {
     position: {
         x :0,
         y :0,
     },
-    velocity:{
-        x :0,
-        y :0,
+    velocity: {
+        x:0,
+        y:10,
     }
-    });
+});
 
-const enemy = new Sprite ({
+const enemy = new Sprite( {
     position: {
-        x :30,
-        y :150,
+        x :400,
+        y :100,
     },
-    velocity:{
-        x :0,
-        y :0,
+    velocity: {
+        x:0,
+        y:0,
     }
-    });
-
+});
 
 console.log(player);
 
-player.draw();
-enemy.draw();
+const keys = {
+    d: {
+        pressed : false,
+    }
+}
+// player.draw();
+// enemy.draw();
 
-
-function animate(){
+function animate() {
     window.requestAnimationFrame(animate);
 
     c.fillStyle = "black";
-    c.fillRect(0,0, canvas,width, canvas.height);
+    c.fillRect(0, 0, canvas.width, canvas.height);
 
-    player.updata();
-    enemy.updata();
+    player.update();
+    enemy.update();
+
+    player.velocity.x = 0;
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+    console.log(event.key);
+
+    switch(event.key){
+        case "d":
+            player.velocity.x =1;
+            break;
+    }
+
+ window.addEventListener("keyup", (event) => {
+    console.log(event.key);
+
+    switch(event.key){
+        case "d":
+            player.velocity.x =1;
+            break;
+    }
+})
